b031e8884729ab60d6ba0b295cb71ed5d37c73df
Don Schemmel <dons500@gmail.com>
2015-08-28 13:04:21 -0700

    Add routes to individual brands page

--- app/app.php
+++ app/app.php
@@ -32,5 +32,17 @@
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+    $app->post("/brands", function() use ($app) {
+        $type = $_POST['type'];
+        $brand = new Brand($type);
+        $brand->save();
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    $app->get("/brands/{id}", function($id) use ($app) {
+        $brand = Brand::find($id);
+        return $app['twig']->render('brand.html.twig', array('brand' => $brand, 'stores' => $brand->getStores(), 'all_stores' => Store::getAll()));
+    });
+
     return $app;
 ?>
